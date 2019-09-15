8b01116361791f8c9f53199d5ce251d8628822bf
mathsalgado <42283985+mathsalgado@users.noreply.github.com>
2019-09-15 17:33:21 -0300

    Adição DBMusicas/ Musicas.java

---
+++ crudMusicas_projeto03/src/java/br/com/fatecpg/musicas/Musicas.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.musicas;
+
+
+
+/**
+ *
+ * @author Usuário
+ */
+public class Musicas {
+    private String Musica;
+    private String Banda;
+    private String Disco;
+    
+    public Musicas (String Banda, String Disco, String Musica) {
+        this.Banda = Banda;
+        this.Disco = Disco;
+        this.Musica = Musica;
+        
+    } 
+
+    public String getMusica() {
+        return Musica;
+    }
+
+    public void setMusica(String Musica) {
+        this.Musica = Musica;
+    }
+
+    public String getBanda() {
+        return Banda;
+    }
+
+    public void setBanda(String Banda) {
+        this.Banda = Banda;
+    }
+
+    public String getDisco() {
+        return Disco;
+    }
+
+    public void setDisco(String Disco) {
+        this.Disco = Disco;
+    }
+}
+
